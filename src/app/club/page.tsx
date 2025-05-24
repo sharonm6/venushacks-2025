adeb383e147304764044493d94aadedd7b6e8d9c
nchowcode <nathanchowcoding@gmail.com>
2025-05-23 22:45:56 -0700

    add: important links, fix page overflow

--- src/app/club/page.tsx
+++ src/app/club/page.tsx
@@ -2,8 +2,10 @@ import ClubBanner from "@/components/club-banner";
 
 export default function Home() {
   return (
-    <div className="mx-auto flex w-full flex-col space-y-8 pt-16">
-      <ClubBanner />
+    <div className="mx-auto flex w-full flex-col space-y-8">
+      <div className="mx-auto flex h-screen w-full flex-col">
+        <ClubBanner />
+      </div>
     </div>
   );
 }
